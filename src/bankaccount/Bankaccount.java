573623672e16f642b0b8bf98962b09c67e3aac03
Angelo <83653389+lqo0opl@users.noreply.github.com>
2023-09-15 08:53:36 +0800

    Update Bankaccount.java

--- src/bankaccount/Bankaccount.java
+++ src/bankaccount/Bankaccount.java
@@ -16,14 +16,20 @@ public class Bankaccount {
      */
     public static void main(String[] args) {
         Account acc = new Account();
-        acc.setAccountnumber(12345);
-        acc.setBalance(10);
-        acc.setName("Failure");
-        acc.setEmail("Bank@gmail.com");
+        Account acc1 = new Account();
+        //1st ACC
+        acc.setAccountnumber(1); acc.setBalance(10);
+        acc.setName("Failure"); acc.setEmail("Bank@gmail.com");
         acc.setPhonenumber(01347);
+        //2nd ACC
+        acc1.setAccountnumber(2);acc1.setBalance(100);
+        acc1.setName("Dunno"); acc1.setEmail("Dork@gmail.com");
+        acc1.setPhonenumber(98);
+        
         System.out.println("Current Balance: "+acc.getBalance()+'$');
         acc.stats(acc.depositFunds(200f));
         acc.stats(acc.withdrawfunds(300f));     
+        acc.stats(acc.transfer(acc1, 210));
+        System.out.println(acc1);
     }
-    
 }
